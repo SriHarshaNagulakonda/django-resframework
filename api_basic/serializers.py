6f11ac45fc3faf864f4cb3e09eb04d8408e74fda
SriHarshaNagulakonda <sriharshanagulakonda@gmail.com>
2021-02-18 18:27:32 +0530

    api_view() Decorator

--- api_basic/serializers.py
+++ api_basic/serializers.py
@@ -5,5 +5,5 @@ class ArticleSerializer(serializers.ModelSerializer):
 
     class Meta:
         model=Article
-        fields=['id','title','author']
+        fields=['id','title','author','email']
 
