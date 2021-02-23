f5ea90717c56be9a84b1c2014cc326860ea34ac9
SriHarshaNagulakonda <sriharshanagulakonda@gmail.com>
2021-02-23 23:20:35 +0530

    Generic views and mixins

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns=[
     path('article/',ArticleAPIView.as_view()),
-    path('article/<int:id>',ArticleDetails.as_view())
+    path('article/<int:id>',ArticleDetails.as_view()),
+    path('generic/article/<int:id>',GenericAPIView.as_view())
 ]
 
